c01a2267e92fdde34c13430006bf79c19719111b
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-10-11 23:02:05 +0400

    added futures for working with files in deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,7 +1,12 @@
+const uuid = require('uuid')
+
 class DeviceController {
     async create(req, res) {
-
+        const {name, price, brandId, typeId, info} = req.body
+        const {img} = req.files
+        let fileName =
     }
+
     async getAll(req, res) {
 
     }
