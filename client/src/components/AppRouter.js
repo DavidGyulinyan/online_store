2a665bf366cf086466f3b4c805fa01b8dbe70df0
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-10-17 00:14:49 +0400

    mobx connected

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,20 +1,22 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { authRoutes, publicRoutes } from '../routes';
-import { SHOP_ROUTE } from '../utils/constants';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import {authRoutes, publicRoutes} from '../routes';
+import {SHOP_ROUTE} from '../utils/constants';
+import {useContext} from "react";
+import {Context} from "../index";
 
 const AppRouter = () => {
-    const isAuth = false;
-
+    const {user} = useContext(Context)
+    console.log(user)
     return (
         <Routes>
-            {isAuth &&
-                authRoutes.map(({ path, Component }) => (
-                    <Route key={path} path={path} element={<Component />} />
+            {user.isAuth &&
+                authRoutes.map(({path, Component}) => (
+                    <Route key={path} path={path} element={<Component/>}/>
                 ))}
-            {publicRoutes.map(({ path, Component }) => (
-                <Route key={path} path={path} element={<Component />} />
+            {publicRoutes.map(({path, Component}) => (
+                <Route key={path} path={path} element={<Component/>}/>
             ))}
-            <Route path="*" element={<Navigate to={SHOP_ROUTE} />} />
+            <Route path="*" element={<Navigate to={SHOP_ROUTE}/>}/>
         </Routes>
     );
 };
