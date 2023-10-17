234b8df28e6af0bfa32cee19636abfd463fcd6a0
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-10-18 01:39:55 +0400

    NavBar implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
 
 function App() {
-    return (
-        <BrowserRouter className="App">
-            <AppRouter/>
-        </BrowserRouter>
-    );
+    return (<BrowserRouter className="App">
+        <NavBar/>
+        <AppRouter/>
+    </BrowserRouter>);
 }
 
 export default App;
