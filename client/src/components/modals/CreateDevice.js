aa4d656461ff6bffce737df42ca07d56b52d387b
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-10-30 16:06:20 +0400

    modal windowses

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,32 @@
-import React from 'react';
-import {Button, Form, Modal} from "react-bootstrap";
+import React, {useContext, useState} from 'react';
+import {
+    Button, Col,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Form,
+    FormControl,
+    Modal,
+    ModalHeader, Row
+} from "react-bootstrap";
+import {Context} from "../../index";
 
 const CreateDevice = ({show, onHide}) => {
+    const {device} = useContext(Context)
+    const [info, setInfo] = useState([])
+
+    const addInfo = () => {
+        setInfo([
+            ...info,
+            {
+                id: new Date().getTime(),
+                title: "",
+                description: "",
+                number: Date.now()
+            }
+        ])
+    }
 
     return (
         <Modal
@@ -10,14 +35,72 @@ const CreateDevice = ({show, onHide}) => {
             size="lg"
             centered
         >
-            <Modal.Header closeButton>
+            <ModalHeader closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
                     Add new device
                 </Modal.Title>
-            </Modal.Header>
+            </ModalHeader>
             <Modal.Body>
                 <Form>
-                    <Form.Control placeholder={"enter the name of type"}/>
+                    <Dropdown className="mt-2">
+                        <DropdownToggle>Choose the type</DropdownToggle>
+                        <DropdownMenu>
+                            {device.types.map(type =>
+                                <DropdownItem key={type.id}>
+                                    {type.name}
+                                </DropdownItem>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <Dropdown className="mt-2">
+                        <DropdownToggle>Choose the brand</DropdownToggle>
+                        <DropdownMenu>
+                            {device.brands.map(brand =>
+                                <DropdownItem key={brand.id}>
+                                    {brand.name}
+                                </DropdownItem>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <FormControl
+                        className="mt-3"
+                        placeholder="Name"
+                    />
+                    <FormControl
+                        className="mt-3"
+                        placeholder="Price"
+                        type="number"
+                    />
+                    <FormControl
+                        className="mt-3"
+                        placeholder="Enter the name of device"
+                        type="file"
+                    />
+                    <hr/>
+
+                    <Button
+                        variant="outline-dark"
+                        onClick={addInfo}
+                    >
+                        Add
+                    </Button>
+                    {info.map(i =>
+                        <Row key={i.id}>
+                            <Col md={4}>
+                                <FormControl
+                                    placeholder="Enter name"
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <FormControl
+                                    placeholder="Enter description"
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <Button variant="outline-danger">Delete</Button>
+                            </Col>
+                        </Row>
+                    )}
                 </Form>
             </Modal.Body>
             <Modal.Footer>
